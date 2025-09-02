40a4bc6da642bbf98eebcacc03d8b0a115d975cc
Oscar Cruz <oscarcruzsena2006@gmail.com>
2025-09-02 07:33:34 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'   // 👈 importante para que cargue Tailwind
+import App from './App'
+import './index.css'   // 👈 IMPORTANTE
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
