8e729260c2d03cf6c01fe84a928f2fc735d42c49
Oscar Cruz <oscarcruzsena2006@gmail.com>
2025-09-02 08:37:37 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react', 'react-dom', 'three', '@react-three/fiber', '@react-three/drei']
+      external: ['three', '@react-three/fiber', '@react-three/drei']
     }
   }
 })
